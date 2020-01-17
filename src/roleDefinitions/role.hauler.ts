40c6230733fdb6a2e6da3ff83aa5f3941391b339
DougHomeDesktop <dwbono01@gmail.com>
2020-01-17 18:54:46 -0500

    hauler priority improvement

--- src/roleDefinitions/role.hauler.ts
+++ src/roleDefinitions/role.hauler.ts
@@ -8,17 +8,16 @@ export class roleHauler {
 
         if (creep.memory.working && currentEnergy == 0) {
             creep.memory.working = false;
-            creep.say('🔄 withdraw');
+            creep.say('🏗️ pickup');
         }
         if (!creep.memory.working && creep.store.getFreeCapacity() == 0) {
             creep.memory.working = true;
-            creep.say('🚧 deposit');
+            creep.say('💦');
         }
 
         //energy full, time to find deposit location.
         if (creep.memory.working) {
-            let target = this.findEnergyDeposits(creep)
-
+            let target = this.findEnergyTopPriorityDeposit(creep)
             if (target) {
                 if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
@@ -31,18 +30,35 @@ export class roleHauler {
                         creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                     }
                 }
+                else {
+                    let target = this.findEnergyLowPriorityDeposit(creep)
+                    if (target) {
+                        if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+                        }
+                    }
+                }
             }
+
         }
         else {
             containerSelector.withdraw(creep);
         }
     }
 
-    private static findEnergyDeposits(creep: Creep) {
+    private static findEnergyTopPriorityDeposit(creep: Creep) {
         return creep.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION
-                    || structure.structureType == STRUCTURE_TOWER) &&
+                return (structure.structureType == STRUCTURE_EXTENSION) &&
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            }
+        });
+    }
+
+    private static findEnergyLowPriorityDeposit(creep: Creep) {
+        return creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_TOWER) &&
                     structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
             }
         });
