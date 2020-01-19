d5a7ffcaa4df3e466806a42719d66d01bcf85bf1
DougHomeDesktop <dwbono01@gmail.com>
2020-01-19 18:55:40 -0500

    added builders to bronze era

--- src/configurable/configurable.BronzeEra.ts
+++ src/configurable/configurable.BronzeEra.ts
@@ -2,8 +2,10 @@ export class BronzeEra {
     public constructor() {
         this.drones = 4;
         this.haulers = 2;
+        this.builders = 2;
     }
     public drones!: number;
     public haulers!: number;
+    public builders!: number;
 }
 
