65d205d203d7bcf776a20c253948f1458c637e2b
DougHomeDesktop <dwbono01@gmail.com>
2020-01-28 17:53:10 -0500

    added container repair to remote dropper

--- src/roleDefinitions/remote/role.remote.dropper.ts
+++ src/roleDefinitions/remote/role.remote.dropper.ts
@@ -3,7 +3,14 @@ import { RoleRemote } from "roleDefinitions/base/role.remote";
 export class RoleRemoteDropper extends RoleRemote {
     public run(creep: Creep) {
         if (creep.memory.working) {
-            super.harvestPrecious(creep);
+            if (super.harvestPrecious(creep) === ERR_NOT_ENOUGH_RESOURCES) {
+                const container: Structure | null = Game.getObjectById(creep.memory.dedication!);
+                if (container) {
+                    if ((container.hitsMax - container.hits) > 100) {
+                        creep.repair(container);
+                    }
+                }
+            }
         }
         else {
             this.dedicatedContainerRelocateRemote(creep, creep.memory.dedication!, creep.memory.orders!.target);
